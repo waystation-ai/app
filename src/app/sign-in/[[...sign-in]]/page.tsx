82b9e77a3cedf39767f351923bca8a0e47f09ed2
Vlad Bugay <v@bougay.com>
2025-02-13 13:53:00 +0200

    Minor UX fix

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <div className="flex flex-col min-h-screen justify-center items-center"><SignIn /></div>
+  return <div className="flex flex-col  mt-4 sm:mt-20 justify-center items-center"><SignIn /></div>
 }
