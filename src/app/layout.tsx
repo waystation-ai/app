ed2995672936c29d3f55cae8105cc2205f953c5b
Vlad Bugay <v@bougay.com>
2025-02-13 12:04:31 +0200

    Bootstrapping Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 
@@ -21,12 +29,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${sourceSans.variable} antialiased`}
-      >
+      <body className={`${sourceSans.variable} antialiased`}>
+        <header className="bg-white/30 backdrop-blur-md border-b border-white/20 px-4 sm:px-6 py-4 flex flex-row justify-between items-center sticky top-0 z-50 gap-4 sm:gap-0">
+          <div className="flex items-center gap-2">
+            <img src="/aurora-circles.svg" alt="WayStation" className="h-8 w-8" />
+            <h1 className="text-2xl font-bold aurora-text">WayStation</h1>
+          </div>
+          <a href="https://forms.gle/ksX4AVNCJbPFr66F6" 
+            className="aurora-btn px-4 py-2 text-sm font-medium rounded-lg hover:scale-105 transition-transform duration-300 w-auto text-center">
+            Get Early Access
+          </a>
+
+        </header>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,7 @@ export default function Home() {
     <div className="h-screen flex flex-col relative">
       <AuroraBackground />
       {/* Header */}
-      <header className="bg-white/30 backdrop-blur-md border-b border-white/20 px-4 sm:px-6 py-4 flex flex-row justify-between items-center sticky top-0 z-50 gap-4 sm:gap-0">
-        <div className="flex items-center gap-2">
-          <img src="/aurora-circles.svg" alt="WayStation" className="h-8 w-8" />
-          <h1 className="text-2xl font-bold aurora-text">WayStation</h1>
-        </div>
-        <a 
-          href="https://forms.gle/ksX4AVNCJbPFr66F6" 
-          className="aurora-btn px-4 py-2 text-sm font-medium rounded-lg 
-                   hover:scale-105 transition-transform duration-300 w-auto text-center"
-        >
-          Get Early Access
-        </a>
-      </header>
+      
 
       {/* Hero Section */}
       <main className="flex-1 flex flex-col">
