17523606eeb70e4bbc035df70f5540c17a0fc14f
Vlad Bugay <vlad@waystation.ai>
2025-02-17 17:27:41 +0200

    Bloody stupid mistake

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ const isApiToolsRoute = createRouteMatcher(['/api(.*)', '/tools(.*)'])
 export default clerkMiddleware(async (auth, request) => {
   const authorization = request.headers.get('Authorization');
   if (authorization)
-    console.log();
+    console.log(authorization);
   
   // Check if this is an API or tools route
   if (!isPublicRoute(request)) {
