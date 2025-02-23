4e76f2a050c3a15a020c6ac06f672a36c1870174
Vlad Bugay <vlad@waystation.ai>
2025-02-23 18:34:49 +0200

    No Aurora background test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
     <html lang="en">
       <body className={`${sora.className} antialiased flex flex-col`}>
         <BodyBackground/>
-        <AuroraBackground/>
         <header className="bg-white/80 rounded-bl-2xl rounded-br-2xl shadow-[0px_2px_16px_0px_rgba(0,0,0,0.08)] border-b border-white backdrop-blur-xl px-4 sm:px-6 py-4 flex flex-row justify-between items-center sticky top-0 z-50 gap-4 sm:gap-0">
           <Link className="flex items-center gap-2" href="/">
             <Image src="/images/logo.svg" width={32} height={32} alt="WayStation" className="h-8 w-8" />
