3a687de4792036c13ed8db50c21299f99a94b9aa
Vlad Bugay <vlad@waystation.ai>
2025-03-05 10:04:33 +0200

    X pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { PostHogProvider } from './providers';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 
 import BodyBackground from './ui/components/BodyBackground';
@@ -21,6 +22,14 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
     <ClerkProvider waitlistUrl="/waitlist">
     <html lang="en">
       <GoogleTagManager gtmId="GTM-TBLJV48V" />
+      <Script id="twitter-pixel" strategy="afterInteractive">
+        {`
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+          twq('config','p82zo');
+        `}
+      </Script>
       <body className={`${sora.className} antialiased flex flex-col`}>
         <PostHogProvider>
           <BodyBackground/>
