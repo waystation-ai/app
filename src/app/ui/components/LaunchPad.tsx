79b1fe85221be28f60dfcc3d8e4ab96ad7e98d3d
Vlad Bugay <vlad@waystation.ai>
2025-03-12 19:15:40 +0200

    Minor tracking adjustment

--- src/app/ui/components/LaunchPad.tsx
+++ src/app/ui/components/LaunchPad.tsx
@@ -11,7 +11,7 @@ export function LaunchPad({ gptId }: LaunchPadProps) {
   const trackEvent = useTrackEvent();
 
   return (
-    <Link href={`https://chatgpt.com/g/${gptId}-waystation`} onClick={() => trackEvent('launchGPT')} target='_blank' className="launch-btn my-2 sm:my-8 px-4 py-2 text-lg font-bold rounded-lg hover:scale-105 transition-transform duration-300 w-auto text-center">
+    <Link href={`https://chatgpt.com/g/${gptId}-waystation`} onClick={() => trackEvent('launch', {'app': "ChatGPT"})} target='_blank' className="launch-btn my-2 sm:my-8 px-4 py-2 text-lg font-bold rounded-lg hover:scale-105 transition-transform duration-300 w-auto text-center">
       <span>Launch WayStation GPT</span>
     </Link>
   )
