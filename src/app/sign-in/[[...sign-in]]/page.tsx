39eaa3b76ad6858917926e54c1607a2cc7d2456f
Vlad Bugay <vlad@waystation.ai>
2025-03-15 12:00:58 +0200

    Canonical for sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 import { Metadata } from 'next';
+import Head from 'next/head';
  
 export const metadata: Metadata = {
   title: 'Sign In or Sign Up | WayStation',
@@ -16,5 +17,10 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <div className="flex flex-col  mt-4 sm:mt-20 justify-center items-center"><SignIn /></div>
+  return <div className="flex flex-col  mt-4 sm:mt-20 justify-center items-center">
+      <Head>
+        <link rel="canonical" href="/sign-in" />
+      </Head>
+      <SignIn />
+    </div>
 }
