42b31c4afbba69df2e8670100e3ac32f4b84bffa
Vlad Bugay <vlad@waystation.ai>
2025-03-15 10:00:59 +0200

    Canonical URL for sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,16 @@ import { SignIn } from '@clerk/nextjs'
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Sign In - WayStation',
+  title: 'Sign In or Sign Up | WayStation',
+  openGraph: {
+    type: 'website',
+    title: "Sign In or Sign Up | WayStation",
+    siteName: "WayStation",
+    url: `/sign-in`,
+    images : {
+      url: '/images/promo-wide.png'
+    }
+  }
 };
 
 export default function Page() {
