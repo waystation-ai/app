ec9c30129904119dd62669023f68fdc5d109498d
Vlad Bugay <vlad@waystation.ai>
2025-03-27 15:45:16 +0200

    Changed order of providers

--- src/app/tools/main.ts
+++ src/app/tools/main.ts
@@ -1,16 +1,21 @@
 // Import all providers
-import './monday';
-import './slack';
-import './gdrive';
-import './gmail';
-import './airtable';
-import './office';
-import './asana';
 import './notion';
+import './monday';
+import './asana';
+import './slack';
+import './airtable';
+import './gdrive';
+import './office';
 import './teams';
+import './miro';
+import './jira';
+import './wrike';
+import './clickup';
+import './linear';
+import './gmail';
+import './gsheets';
 import './mailchimp';
 import './zoom';
-import './jira';
 import './hubspot';
 import './trello';
 import './outlook';
@@ -18,11 +23,7 @@ import './chrome';
 import './gmeet';
 import './salesforce';
 import './smartsheet';
-import './miro';
-import './wrike';
-import './clickup';
-import './linear';
-import './gsheets';
+
 
 // This file serves as the main entry point for all tools
 // It imports and registers all providers, making them available to the system
